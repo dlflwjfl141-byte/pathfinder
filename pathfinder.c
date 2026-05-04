1a0b1e823f36966413435ad9f4439e79bb7610c9
dlflwjfl <dlflwjfl141@gmail.com>
2026-05-05 05:07:14 +0900

    Add files via upload

---
+++ pathfinder.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int visited[100][100];
+int dist[100][100];
+int map[100][100];
+int steps[100][100];
+
+int dr[4] = {-1, 1, 0, 0};
+int dc[4] = {0, 0, -1, 1};
+
+struct queue
+{
+    int queue[1000];
+    int front;
+    int back;
+};
+struct queue qx;
+struct queue qy;
+
+void q_init(struct queue *q)
+{
+    q->front = 0;
+    q->back = 0;
+}
+
+void q_push(struct queue *q, int x)
+{
+    q->queue[q->back++] = x;
+}
+
+int q_pop(struct queue *q)
+{
+    return q->queue[q->front++];
+}
+
+int q_empty(struct queue *q)
+{
+    return q->front == q->back;
+}
+
+void pop_min(struct queue *qx, struct queue *qy, int *x, int *y)
+{
+    int min_idx = qx->front;
+    int min_cost = dist[qx->queue[min_idx]][qy->queue[min_idx]];
+
+    for (int i = qx->front; i < qx->back; i++)
+    {
+        int cx = qx->queue[i];
+        int cy = qy->queue[i];
+
+        if (dist[cx][cy] < min_cost)
+        {
+            min_cost = dist[cx][cy];
+            min_idx = i;
+        }
+    }
+
+    *x = qx->queue[min_idx];
+    *y = qy->queue[min_idx];
+
+    // min_idx 위치를 마지막 원소로 덮어서 삭제
+    qx->queue[min_idx] = qx->queue[qx->back - 1];
+    qy->queue[min_idx] = qy->queue[qy->back - 1];
+
+    qx->back--;
+    qy->back--;
+}
+
+int main()
+{
+
+    q_init(&qx);
+    q_init(&qy);
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            scanf("%d", &map[i][j]);
+            visited[i][j] = 0;
+        }
+    }
+    int cost = 100;
+    int x;
+    int y;
+
+    visited[0][0] = 1;
+    dist[0][0] = 0;
+    q_push(&qx, 0);
+    q_push(&qy, 0);
+    while (q_empty(&qx) == 0)
+    {
+        pop_min(&qx, &qy, x, y);
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + dr[i];
+            int ny = y + dc[i];
+            if (nx < 0 || nx >= 10 || ny < 0 || ny >= 10)
+                continue;
+            if (map[nx][ny] == 0)
+                continue;
+
+            cost = dist[x][y] + map[nx][ny];
+
+            if (dist[nx][ny] > cost)
+            {
+                dist[nx][ny] = cost;
+                steps[nx][ny] = steps[x][y] + 1;
+                
+                q_push(&qx, nx);
+                q_push(&qy, ny);
+            }
+        }
+    }
+}
