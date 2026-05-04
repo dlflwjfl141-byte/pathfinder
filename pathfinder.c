cb9e618b6051d06a0b5163e827860b3a7bea6196
dlflwjfl <dlflwjfl141@gmail.com>
2026-05-05 06:23:03 +0900

    Refactor pathfinder.c for Dijkstra's algorithm
    Refactor pathfinding algorithm to implement Dijkstra's algorithm with additional features for tracking parent nodes and costs.

--- pathfinder.c
+++ pathfinder.c
@@ -2,10 +2,15 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define INF 987654321
+
 int visited[100][100];
 int dist[100][100];
 int map[100][100];
+int copymap[100][100];
 int steps[100][100];
+int parentx[100][100];
+int parenty[100][100];
 
 int dr[4] = {-1, 1, 0, 0};
 int dc[4] = {0, 0, -1, 1};
@@ -67,52 +72,150 @@ void pop_min(struct queue *qx, struct queue *qy, int *x, int *y)
     qx->back--;
     qy->back--;
 }
-
-int main()
+void input_map()
 {
-
-    q_init(&qx);
-    q_init(&qy);
-
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
             scanf("%d", &map[i][j]);
             visited[i][j] = 0;
+            dist[i][j] = INF;
+            steps[i][j] = INF;
         }
     }
-    int cost = 100;
+}
+void dijkstra()
+{
+    if (map[0][0] == 0 || map[9][9] == 0)
+    {
+        return;
+    }
+    q_init(&qx);
+    q_init(&qy);
+
     int x;
     int y;
 
-    visited[0][0] = 1;
     dist[0][0] = 0;
+    steps[0][0] = 0;
+
     q_push(&qx, 0);
     q_push(&qy, 0);
+
     while (q_empty(&qx) == 0)
     {
-        pop_min(&qx, &qy, x, y);
+        pop_min(&qx, &qy, &x, &y);
+
+        if (visited[x][y] == 1)
+            continue;
+
+        visited[x][y] = 1;
 
         for (int i = 0; i < 4; i++)
         {
             int nx = x + dr[i];
             int ny = y + dc[i];
+
             if (nx < 0 || nx >= 10 || ny < 0 || ny >= 10)
                 continue;
+
+            if (visited[nx][ny] == 1)
+                continue;
+
             if (map[nx][ny] == 0)
                 continue;
 
-            cost = dist[x][y] + map[nx][ny];
+            int cost = dist[x][y] + map[nx][ny];
 
             if (dist[nx][ny] > cost)
             {
                 dist[nx][ny] = cost;
                 steps[nx][ny] = steps[x][y] + 1;
-                
+
+                parentx[nx][ny] = x;
+                parenty[nx][ny] = y;
+
                 q_push(&qx, nx);
                 q_push(&qy, ny);
             }
         }
     }
+}
+void init_arrays()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            dist[i][j] = INF;
+            steps[i][j] = INF;
+        }
+    }
+}
+void rev(int x, int y)
+{
+
+    while (!(x == 0 && y == 0))
+    {
+        if (!(x == 9 && y == 9))
+            copymap[x][y] = -2;
+
+        int px = parentx[x][y];
+        int py = parenty[x][y];
+
+        x = px;
+        y = py;
+    }
+    copymap[0][0] = -3;
+    copymap[9][9] = -4;
+}
+void result()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            copymap[i][j] = map[i][j];
+        }
+    }
+
+    if (dist[9][9] == INF)
+    {
+        printf("도착 불가능\n");
+        return;
+    }
+    else
+    {
+        printf("최소 비용: %d\n", dist[9][9]);
+        printf("이동 거리: %d\n", steps[9][9]);
+    }
+    rev(9, 9);
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (copymap[i][j] == -3)
+                printf("S ");
+            else if (copymap[i][j] == -4)
+                printf("G ");
+            else if (copymap[i][j] == -2)
+                printf("* ");
+            else
+                printf("%d ", copymap[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+
+    input_map();
+
+    init_arrays();
+
+    dijkstra();
+
+    result();
 }
