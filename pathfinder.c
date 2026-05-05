ad929d56cf1a4777fd2acce305aa866f88b48b40
dlflwjfl <dlflwjfl141@gmail.com>
2026-05-06 02:27:23 +0900

    Refactor pathfinding code to include BFS and Dijkstra

--- pathfinder.c
+++ pathfinder.c
@@ -4,13 +4,22 @@
 
 #define INF 987654321
 
-int visited[100][100];
-int dist[100][100];
+int dijkDist[100][100];
+int dijkSteps[100][100];
+int dijkVisited[100][100];
+
+int bfsDist[100][100];
+int bfsVisited[100][100];
+
 int map[100][100];
-int copymap[100][100];
-int steps[100][100];
-int parentx[100][100];
-int parenty[100][100];
+int dijkmap[100][100];
+int BFSmap[100][100];
+
+int dijkparentx[100][100];
+int dijkparenty[100][100];
+
+int bfsparentx[100][100];
+int bfsparenty[100][100];
 
 int dr[4] = {-1, 1, 0, 0};
 int dc[4] = {0, 0, -1, 1};
@@ -21,8 +30,7 @@ struct queue
     int front;
     int back;
 };
-struct queue qx;
-struct queue qy;
+struct queue qx, qy;
 
 void q_init(struct queue *q)
 {
@@ -48,16 +56,16 @@ int q_empty(struct queue *q)
 void pop_min(struct queue *qx, struct queue *qy, int *x, int *y)
 {
     int min_idx = qx->front;
-    int min_cost = dist[qx->queue[min_idx]][qy->queue[min_idx]];
+    int min_cost = dijkDist[qx->queue[min_idx]][qy->queue[min_idx]];
 
     for (int i = qx->front; i < qx->back; i++)
     {
         int cx = qx->queue[i];
         int cy = qy->queue[i];
 
-        if (dist[cx][cy] < min_cost)
+        if (dijkDist[cx][cy] < min_cost)
         {
-            min_cost = dist[cx][cy];
+            min_cost = dijkDist[cx][cy];
             min_idx = i;
         }
     }
@@ -79,26 +87,27 @@ void input_map()
         for (int j = 0; j < 10; j++)
         {
             scanf("%d", &map[i][j]);
-            visited[i][j] = 0;
-            dist[i][j] = INF;
-            steps[i][j] = INF;
+            dijkVisited[i][j] = 0;
+            dijkDist[i][j] = INF;
+            dijkSteps[i][j] = INF;
         }
     }
 }
 void dijkstra()
 {
+    struct queue qx, qy;
+    q_init(&qx);
+    q_init(&qy);
+
     if (map[0][0] == 0 || map[9][9] == 0)
     {
         return;
     }
-    q_init(&qx);
-    q_init(&qy);
-
     int x;
     int y;
 
-    dist[0][0] = 0;
-    steps[0][0] = 0;
+    dijkDist[0][0] = 0;
+    dijkSteps[0][0] = 0;
 
     q_push(&qx, 0);
     q_push(&qy, 0);
@@ -107,10 +116,10 @@ void dijkstra()
     {
         pop_min(&qx, &qy, &x, &y);
 
-        if (visited[x][y] == 1)
+        if (dijkVisited[x][y] == 1)
             continue;
 
-        visited[x][y] = 1;
+        dijkVisited[x][y] = 1;
 
         for (int i = 0; i < 4; i++)
         {
@@ -120,21 +129,21 @@ void dijkstra()
             if (nx < 0 || nx >= 10 || ny < 0 || ny >= 10)
                 continue;
 
-            if (visited[nx][ny] == 1)
+            if (dijkVisited[nx][ny] == 1)
                 continue;
 
             if (map[nx][ny] == 0)
                 continue;
 
-            int cost = dist[x][y] + map[nx][ny];
+            int cost = dijkDist[x][y] + map[nx][ny];
 
-            if (dist[nx][ny] > cost)
+            if (dijkDist[nx][ny] > cost)
             {
-                dist[nx][ny] = cost;
-                steps[nx][ny] = steps[x][y] + 1;
+                dijkDist[nx][ny] = cost;
+                dijkSteps[nx][ny] = dijkSteps[x][y] + 1;
 
-                parentx[nx][ny] = x;
-                parenty[nx][ny] = y;
+                dijkparentx[nx][ny] = x;
+                dijkparenty[nx][ny] = y;
 
                 q_push(&qx, nx);
                 q_push(&qy, ny);
@@ -142,80 +151,191 @@ void dijkstra()
         }
     }
 }
-void init_arrays()
+void init_diarrays()
 {
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
-            dist[i][j] = INF;
-            steps[i][j] = INF;
+            dijkDist[i][j] = INF;
+            dijkSteps[i][j] = INF;
         }
     }
 }
-void rev(int x, int y)
+void rev_dijk(int x, int y)
 {
 
     while (!(x == 0 && y == 0))
     {
         if (!(x == 9 && y == 9))
-            copymap[x][y] = -2;
+            dijkmap[x][y] = -2;
 
-        int px = parentx[x][y];
-        int py = parenty[x][y];
+        int px = dijkparentx[x][y];
+        int py = dijkparenty[x][y];
 
         x = px;
         y = py;
     }
-    copymap[0][0] = -3;
-    copymap[9][9] = -4;
+    dijkmap[0][0] = -3;
+    dijkmap[9][9] = -4;
 }
-void result()
+
+void rev_bfs(int x,int y){
+    while (!(x == 0 && y == 0))
+    {
+        if (!(x == 9 && y == 9))
+            BFSmap[x][y] = -2;
+
+        int px = bfsparentx[x][y];
+        int py = bfsparenty[x][y];
+
+        x = px;
+        y = py;
+    }
+    BFSmap[0][0] = -3;
+    BFSmap[9][9] = -4;
+}
+void result_dijk()
 {
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
-            copymap[i][j] = map[i][j];
+            dijkmap[i][j] = map[i][j];
         }
     }
 
-    if (dist[9][9] == INF)
+    if (dijkDist[9][9] == INF)
     {
         printf("도착 불가능\n");
         return;
     }
     else
     {
-        printf("최소 비용: %d\n", dist[9][9]);
-        printf("이동 거리: %d\n", steps[9][9]);
+        printf("최소 비용: %d\n", dijkDist[9][9]);
+        printf("이동 거리: %d\n", dijkSteps[9][9]);
     }
-    rev(9, 9);
+    rev_dijk(9, 9);
+    printf("\n다익스트라 지도\n");
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
-            if (copymap[i][j] == -3)
+            if (dijkmap[i][j] == -3)
                 printf("S ");
-            else if (copymap[i][j] == -4)
+            else if (dijkmap[i][j] == -4)
                 printf("G ");
-            else if (copymap[i][j] == -2)
+            else if (dijkmap[i][j] == -2)
                 printf("* ");
             else
-                printf("%d ", copymap[i][j]);
+                printf("%d ", dijkmap[i][j]);
         }
         printf("\n");
     }
 }
+void result_bfs()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)        
+        {
+            BFSmap[i][j] = map[i][j];
+        }
+    }
+    if (bfsDist[9][9] == INF)
+    {
+        printf("도착 불가능\n");
+        return;
+    }
+    else
+    {
+        printf("최단 이동 거리: %d\n", bfsDist[9][9]);
+    }
+    rev_bfs(9,9);
+    printf("\nBFS 지도\n");
 
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (BFSmap[i][j] == -3)
+                printf("S ");
+            else if (BFSmap[i][j] == -4)
+                printf("G ");
+            else if (BFSmap[i][j] == -2)
+                printf("* ");
+            else
+                printf("%d ", BFSmap[i][j]);
+        }
+        printf("\n");
+    }
+}
+void init_bfsarray()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            bfsDist[i][j] = INF;
+            bfsVisited[i][j] = 0;
+        }
+    }
+}
+void BFS()
+{
+    struct queue qx, qy;
+    q_init(&qx);
+    q_init(&qy);
+
+    if (map[0][0] == 0 || map[9][9] == 0)
+        return;
+
+    q_push(&qx, 0);
+    q_push(&qy, 0);
+
+    bfsDist[0][0] = 0;
+    bfsVisited[0][0] = 1;
+    while (q_empty(&qx) == 0)
+    {
+        int x = q_pop(&qx);
+        int y = q_pop(&qy);
+        for (int i = 0; i < 4; i++)
+        {
+            
+            int nx = x + dr[i];
+            int ny = y + dc[i];
+
+            if (nx < 0 || nx >= 10 || ny < 0 || ny >= 10)
+                continue;
+
+            if (map[nx][ny] == 0)
+                continue;
+
+            if (bfsVisited[nx][ny] == 1)
+                continue;
+
+            bfsVisited[nx][ny] = 1;
+            bfsDist[nx][ny] = bfsDist[x][y] + 1;
+
+            bfsparentx[nx][ny] = x;
+            bfsparenty[nx][ny] = y;
+
+            q_push(&qx, nx);
+            q_push(&qy, ny);
+        }
+    }
+}
 int main()
 {
 
     input_map();
 
-    init_arrays();
+    init_diarrays();
+    init_bfsarray();
 
     dijkstra();
+    BFS();
 
-    result();
+    result_dijk();
+    result_bfs();
 }
